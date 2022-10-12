eb6877070e4dc5207ca9a44c7b2e116bd48285dd
ash0545 <homeashwinsanthosh@gmail.com>
2022-10-12 15:26:53 +0530

    Add function to play a single round

--- javascript.js
+++ javascript.js
@@ -17,11 +17,46 @@ Play a round of rock paper scissor:
 // Get player's choice
 let getPlayerChoice = prompt("Enter your choice : ").toLowerCase()
 console.log(getPlayerChoice);
+
 //Get computer's random choice using a function
 function getComputerChoice() {
     let x = Math.floor(3 * Math.random());
     let choice = ["rock", "paper", "scissor"];
+    console.log(choice[x]);
     return choice[x];
 }
 
-console.log(getComputerChoice());
+/*
+Function to play a single round, getting both player and computer
+inputs and returning the result.
+*/
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("It's a draw!");
+    } else {
+        if (playerSelection === "rock") {
+            if (computerSelection === "scissor") {
+                return "You win, Rock beats Scissors";
+            } else {
+                return "You lose, Paper beats Rock";
+            }
+        } else if (playerSelection === "paper") {
+            if (computerSelection === "rock") {
+                return "You win, Paper beats Rock";
+            } else {
+                return "You lose, Scissor beats Paper";
+            }
+        } else if (playerSelection === "scissor") {
+            if (computerSelection === "paper") {
+                return "You win, Scissor beats Paper"
+            } else {
+                return "You lose, Rock beats Scissor";
+            }
+        } else {
+            return "Invalid choice";
+        }
+    }
+}
+
+console.log(playRound(getPlayerChoice, getComputerChoice()));
