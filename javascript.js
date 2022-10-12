9bbeebdcd4331ce1f6f7490b8e2b8306b243da4f
ash0545 <homeashwinsanthosh@gmail.com>
2022-10-12 14:57:52 +0530

    Add function to get computer's choice

---
+++ javascript.js
@@ -0,0 +1,25 @@
+/*
+Play a round of rock paper scissor:
+    Take in player's choice and computer's choice
+    If player and computer same choice:
+        Draw
+    Else:
+        If P R:
+            If C S:
+                You win
+            Else
+                You lose
+        If P S:
+            If C R:
+        ...
+
+*/
+
+//Get computer's random choice using a function
+function getComputerChoice() {
+    let x = Math.floor(3 * Math.random());
+    let choice = ["rock", "paper", "scissor"];
+    return choice[x];
+}
+
+console.log(getComputerChoice());
